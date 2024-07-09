12979a4128c94c5053b76b2988bd33cfba9c1dac
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2024-07-09 16:55:54 +0300

    bencoding: completed and now testing

--- bencoding.go
+++ bencoding.go
@@ -4,34 +4,149 @@ import (
 	"bufio"
 	"errors"
 	"io"
+	"strconv"
 )
 
 type BencodingDecoder struct {
 	r         *bufio.Reader
 	bytesRead int
+	data      map[string]any
 }
 
 func NewDecoder(r io.Reader) *BencodingDecoder {
 	return &BencodingDecoder{r: bufio.NewReader(r), bytesRead: 0}
 }
 
-func (d *BencodingDecoder) decodeInt() error {
+func (d *BencodingDecoder) DecodeDict() (value map[any]any, err error) {
 	ch, err := d.readByte()
 	if err != nil {
-		return err
+		return
+	}
+	if ch != 'd' {
+		panic("expected 'd' for dictionary")
+	}
+	value = make(map[any]any)
+	next, err := d.peek()
+	if err != nil {
+		return
+	}
+	var tempHolder []any
+	for next != 'e' {
+		switch next {
+		case 'i':
+			i, ierr := d.DecodeInt()
+			if ierr != nil {
+				return
+			}
+			tempHolder = append(tempHolder, i)
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			s, strerr := d.DecodeString()
+			if strerr != nil {
+				return
+			}
+			tempHolder = append(tempHolder, s)
+		case 'l':
+			l, lerr := d.DecodeList()
+			if lerr != nil {
+				return
+			}
+			tempHolder = append(tempHolder, l)
+		case 'd':
+			d, derr := d.DecodeDict()
+			if derr != nil {
+				return
+			}
+			tempHolder = append(tempHolder, d)
+		}
+		if len(tempHolder) == 2 {
+			value[tempHolder[0]] = tempHolder[1]
+			tempHolder = nil
+		}
+		next, err = d.peek()
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (d *BencodingDecoder) DecodeString() (value string, err error) {
+	strLen, err := d.readBytes(':')
+	if err != nil {
+		return "", err
+	}
+	len, err := strconv.Atoi(string(strLen[:len(strLen)-1]))
+	if err != nil {
+		return "", err
+	}
+	stringBuf := make([]byte, len)
+	buf, err := d.read(stringBuf)
+	if buf != len {
+		panic("Buffer reading error")
+	}
+	if err != nil {
+		return "", err
+	}
+	value = string(stringBuf)
+	return
+}
+
+func (d *BencodingDecoder) DecodeInt() (value int, err error) {
+	ch, err := d.readByte()
+	if err != nil {
+		return -1, err
 	}
 	if ch != 'i' {
 		panic("Expected i for Integer")
 	}
 	line, err := d.readBytes('e')
 	if err != nil {
-		return err
+		return -1, err
 	}
-	digits := string(line[:len(line)-1])
-	return nil
+	value, err = strconv.Atoi(string(line[:len(line)-1]))
+	return
 }
 
-func (d *BencodingDecoder) Decode() string {
+func (d *BencodingDecoder) DecodeList() (value []any, err error) {
+	ch, err := d.readByte()
+	if ch != 'l' {
+		panic("Expected l for List")
+	}
+	next, err := d.peek()
+	if err != nil {
+		return
+	}
+	for next != 'e' {
+		switch next {
+		case 'i':
+			i, ierr := d.DecodeInt()
+			if ierr != nil {
+				return
+			}
+			value = append(value, i)
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+			s, strerr := d.DecodeString()
+			if strerr != nil {
+				return
+			}
+			value = append(value, s)
+		case 'l':
+			l, lerr := d.DecodeList()
+			if lerr != nil {
+				return
+			}
+			value = append(value, l)
+
+		}
+		next, err = d.peek()
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+func (d *BencodingDecoder) DecodeToJSON() string {
 	next, err := d.peek()
 	if err != nil {
 		return ""
---
+++ bencoding_test.go
@@ -0,0 +1,74 @@
+package bazbittorrent_test
+
+import (
+	"reflect"
+	"slices"
+	"strings"
+	"testing"
+
+	bazbittorrent "github.com/CalvoM/baz_bit-torrent"
+)
+
+func TestSimple(t *testing.T) {
+	actual := 2 + 2
+	expected := 4
+	if actual != expected {
+		t.Errorf("got %q, wanted %q", actual, expected)
+	}
+}
+
+func TestDecodeString(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{{"4:spam", "spam"}, {"5:oceans", "ocean"}, {"6:eleven", "eleven"}, {"1:erft", "e"}}
+	for _, test := range tests {
+		bencoding := bazbittorrent.NewDecoder(strings.NewReader(test.input))
+		actual, _ := bencoding.DecodeString()
+		if actual != test.expected {
+			t.Errorf("got %q, wanted %q", actual, test.expected)
+		}
+	}
+}
+
+func TestDecodeInt(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int
+	}{{"i90e", 90}, {"i5e", 5}, {"i1000e", 1000}, {"i9e", 9}}
+	for _, test := range tests {
+		bencoding := bazbittorrent.NewDecoder(strings.NewReader(test.input))
+		actual, _ := bencoding.DecodeInt()
+		if actual != test.expected {
+			t.Errorf("got %q, wanted %q", actual, test.expected)
+		}
+	}
+}
+
+func TestDecodeList(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []any
+	}{{"li90ei1ei12345eei8e", []any{90, 1, 12345}}, {"li90e4:spame", []any{90, "spam"}}}
+	for _, test := range tests {
+		bencoding := bazbittorrent.NewDecoder(strings.NewReader(test.input))
+		actual, _ := bencoding.DecodeList()
+		if !slices.Equal(actual, test.expected) {
+			t.Errorf("got %q, wanted %q", actual, test.expected)
+		}
+	}
+}
+
+func TestDecodeDict(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected map[any]any
+	}{{"d1:a1:be", map[any]any{"a": "b"}}, {"d5:namesl4:sean5:shawni5ee", map[any]any{"names": []any{"sean", "shawn", 5}}}}
+	for _, test := range tests {
+		bencoding := bazbittorrent.NewDecoder(strings.NewReader(test.input))
+		actual, _ := bencoding.DecodeDict()
+		if !reflect.DeepEqual(actual, test.expected) {
+			t.Errorf("got %q, wanted %q", actual, test.expected)
+		}
+	}
+}
